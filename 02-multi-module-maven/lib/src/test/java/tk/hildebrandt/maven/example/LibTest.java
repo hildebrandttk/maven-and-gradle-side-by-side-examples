3a18f8a73960e6ba1d82243ae73cab4fdcbce66b
Stefan Hildebrandt <stefan.hildebrandt.ext@ewe.de>
2023-05-17 20:06:08 +0200

    Rename maven modules and fix package.

--- 02-multi-module-maven/lib/src/test/java/tk/hildebrandt/maven/example/LibTest.java
+++ 02-multi-module-maven/lib/src/test/java/tk/hildebrandt/maven/example/LibTest.java
@@ -1,7 +1,6 @@
-package tk.hildebrandt.gradle.example;
+package tk.hildebrandt.maven.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class LibTest {
 
